a3681f294c962db7404dfc84382275bca0591b35
Dicxie Madrigal <hidannara@yahoo.es>
2021-09-10 09:12:26 -0600

    se agrego la funcion add al archivo Example01.kt

--- src/main/kotlin/Example01.kt
+++ src/main/kotlin/Example01.kt
@@ -4,5 +4,9 @@
  * this progras disply "Hellow, World" in the console
  */
 fun main(args : Array<String>){
-    print("Hello, World ")
+    print("Hello, World " + add(3, 4));
+}
+
+fun add(x : Int, y : Int) : Int{
+    return x + y;
 }
